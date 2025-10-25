e43206ae25504a9047cfcd78f1b264776115e091
omar <omar@>
2025-10-25 18:35:46 +0200

    First commit

---
+++ src/main/java/com/ra2/users/spring_jdbc_users/SpringJdbcUsersApplication.java
@@ -0,0 +1,13 @@
+package com.ra2.users.spring_jdbc_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ra2/users/spring_jdbc_users/SpringJdbcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ra2.users.spring_jdbc_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
