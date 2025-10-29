ea0c733dd2852b53e4bedaf1370d70446bdeba49
omar <omar@>
2025-10-29 19:07:23 +0100

    method get all done

--- src/main/java/com/ra2/users/spring_jdbc_users/controller/UsuarisController.java
+++ src/main/java/com/ra2/users/spring_jdbc_users/controller/UsuarisController.java
@@ -7,12 +7,16 @@ import com.ra2.users.spring_jdbc_users.model.Usuari;
 import com.ra2.users.spring_jdbc_users.repository.UsuariRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -26,6 +30,19 @@ public class UsuarisController {
         usuariRepository.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body("Usuario creado correctamente");
     }
+    
+    @GetMapping("users")
+    public ResponseEntity<List<Usuari>> getAllUsers() {
+        List<Usuari> usuaris =  usuariRepository.findAll();
+        if(usuaris == null || usuaris.isEmpty()){
+            return ResponseEntity.ok(null);
+        }else{
+            return ResponseEntity.ok(usuaris);
+        }
+        
+
+    }
+    
     
     
     
